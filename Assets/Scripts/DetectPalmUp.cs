c103b921d7da4526ccba85cff98956e76b975094
Brendan Lau <brendanwlau@gmail.com>
2021-03-02 01:38:57 -0800

    Deteching palm up to show menu works

--- Assets/Scripts/DetectPalmUp.cs
+++ Assets/Scripts/DetectPalmUp.cs
@@ -7,6 +7,9 @@ using UnityEngine;
  */
 public class DetectPalmUp : MonoBehaviour
 {
+    public GameObject radialMenu;
+    public GameObject userHead;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,17 @@ public class DetectPalmUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Check the orientation of the hand to determine if the palm is facing upwards
+        Vector3 orientation = transform.rotation.eulerAngles;
+        // If the y orientation is within 80 and 100 (upwards is 90), then show the menu
+        if (orientation.z >= 50 && orientation.z <= 130)
+        {
+            radialMenu.SetActive(true);
+
+            // Make the radial menu always look at the user
+            // radialMenu.transform.LookAt(userHead.transform);
+        }
+        else
+            radialMenu.SetActive(false);
     }
 }
